d289e2abf86c8622630c5afca7231d8c516ee942
gogotanaka <qlli.illb@gmail.com>
2014-08-18 14:34:25 -0700

    [MISC] Add echo to server.sh

--- apps/script/assets.sh
+++ apps/script/assets.sh
@@ -1,2 +1,5 @@
+echo 'Compile coffee....'
 coffee -c apps/static/js/application.coffee
+
+echo 'Compile sass....'
 sass --style compressed apps/static/css/application.scss apps/static/css/application.css
