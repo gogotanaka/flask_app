a43176412188f277c73fefb1a70c54c9a65431cc
Kousuke Takeuchi <u651601f@gmail.com>
2014-08-19 22:54:47 -0700

    added remember me functionally

--- apps/controllers/user_controller.py
+++ apps/controllers/user_controller.py
@@ -16,4 +16,15 @@ def register():
 def login():
 	if request.method == 'GET':
 		return render_template('users/login.html')
-	return redirect(url_for('index'))
+	username = request.form['username']
+	password = request.form['password']
+	remember_me = False
+	if 'remember_me' in request.form:
+		remember_me = True
+	registered_user = User.query.filter_by(username=username, password=password).first()
+	if registered_user is None:
+		flash('Username or Password is invalid', 'error')
+		return redirect(url_for('login'))
+	login_user(registered_user, remember = remember_me)
+	flash('Logged in successfully')
+	return redirect(request.args.get('next') or url_for('index'))
