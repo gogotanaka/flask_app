a03e78f60b4c4aff441f37284e94e3c13b1a727d
Kousuke Takeuchi <u651601f@gmail.com>
2014-08-19 21:05:34 -0700

    added templates for login and register also added register code

--- apps/controllers/user_controller.py
+++ apps/controllers/user_controller.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
 from apps import app
 from flask import render_template, request
+
+@app.route('/users/register', methods=['GET', 'POST'])
+def register():
+	if request.method == 'GET':
+		return render_template('users/register.html')
+	user = User(request.form['username'], request.form['password'], request.form['email'])
+	db.session.add(user)
+	db.session.commit()
+	flash('User successfully registered')
+	return redirect(url_for('login'))
+
+@app.route('/users/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'GET':
+		return render_template('users/login.html')
+	return redirect(url_for('index'))
