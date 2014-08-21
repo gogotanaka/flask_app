bf12024ce71b11fd8ee647e36f8727f6c90a03c2
Kousuke Takeuchi <u651601f@gmail.com>
2014-08-20 20:42:58 -0700

    include bootstrap stylesheets

--- apps/__init__.py
+++ apps/__init__.py
@@ -4,12 +4,9 @@ from flask_debugtoolbar import DebugToolbarExtension
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:////db/pakopako.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:////apps/db/pakopako.sqlite3'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-
-(u'sample', u'password', u'sample@example.com', '2014-08-20 20:54:14.820484')
-
 
 app.config['SECRET_KEY'] = 'random'
 app.debug = True
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-from apps import app
+from apps import app, db
+from apps.models.user import User
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 3000))
+
 	app.run('0.0.0.0', port=port)
